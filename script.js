7a0627d99f9218784223c00896410d5ab2462555
sachintha seneviratna <staphanwink@gmail.com>
2025-04-15 16:43:58 +0530

    second commit

--- script.js
+++ script.js
@@ -128,3 +128,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+
+// Navbar shrink on scroll
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('shrink');
+    } else {
+        header.classList.remove('shrink');
+    }
+});
